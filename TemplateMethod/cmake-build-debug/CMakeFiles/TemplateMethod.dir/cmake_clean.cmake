a4a42a3e9dee6112c871739a5f7aa8250ee74d23
Bowen Li <skl7028320@outlook.com>
2021-05-09 00:00:17 +0200

    ignore .idea folders

---
+++ TemplateMethod/cmake-build-debug/CMakeFiles/TemplateMethod.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemplateMethod.dir/main.cpp.o"
+  "TemplateMethod"
+  "TemplateMethod.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemplateMethod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
